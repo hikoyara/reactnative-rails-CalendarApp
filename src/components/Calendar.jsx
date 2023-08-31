8f603ab610b0a911a6d6b5c3d6c2280799ebc420
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-08-31 13:55:13 +0900

    react-native-big-calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,60 +1,18 @@
-import React from "react";
-import { Calendar, LocaleConfig } from "react-native-calendars";
-/* utils */
-import { WindowSize } from "../utils/WindowSize";
+import React, { useState } from "react";
+import { Text } from "react-native";
+import { Calendar } from "react-native-big-calendar";
 
 export default function App(props) {
     const { events } = props;
 
-    return (
-        <Calendar
-            style={{
-                width: WindowSize().width,
-                height: "100%",
-            }}
-            theme={{
-                "stylesheet.calendar.main": {
-                    monthView: {
-                        flex: 1,
-                        height: "100%",
-                        justifyContent: "space-around",
-                    },
-                    week: {
-                        flex: 1,
-                        marginVertical: 0,
-                        flexDirection: "row",
-                        justifyContent: "space-around",
-                    },
-                    dayContainer: {
-                        borderColor: "#f5f5f5",
-                        borderWidth: 1,
-                        flex: 1,
-                    },
-                },
-            }}
-            hideArrows={true}
-            enableSwipeMonths={true}
-            markingType="multi-period"
-            markedDates={{
-                "2023-08-13": {
-                    periods: [
-                        { startingDay: false, endingDay: true, color: "#5f9ea0" },
-                        { startingDay: false, endingDay: true, color: "#ffa500" },
-                        { startingDay: true, endingDay: false, color: "#f0e68c" },
-                    ],
-                },
-                "2023-08-18": {
-                    periods: [{ startingDay: true, endingDay: false, color: "#ffa500" }, { color: "transparent" }, { startingDay: false, endingDay: false, color: "#f0e68c" }],
-                },
-            }}
-        />
-    );
-}
+    const eventsArray = [];
+    events.map((event) => {
+        eventsArray.push({
+            title: event.title,
+            start: new Date(event.start_date),
+            end: new Date(event.end_date),
+        });
+    });
 
-LocaleConfig.locales.jp = {
-    monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
-    monthNamesShort: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
-    dayNames: ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
-    dayNamesShort: ["日", "月", "火", "水", "木", "金", "土"],
-};
-LocaleConfig.defaultLocale = "jp";
+    return <Calendar events={eventsArray} height={100} mode="month" />;
+}
